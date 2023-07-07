1ef3e07677ecda1ecc483623f021cc61f8fa7b7c
lalit kumar <106720144+lalitkumar2597@users.noreply.github.com>
2023-07-07 19:49:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes 
 app.use("/api/v1/auth", authRoutes);
@@ -35,7 +35,7 @@ app.use("/api/v1/product", productRoute);
 
 //Api 
 app.use('*', function (req, res) {
-  res.sendFile(path.join(__dirname,'./client/build/index.html '))
+  res.sendFile(path.join(__dirname,"./client/build/index.html "));
 })
 
 // Port
